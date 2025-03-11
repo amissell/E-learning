99ee11930d9dcfc23eec58c95c741e6d6b003288
amissell <issellaya@gmail.com>
2025-03-11 15:56:19 +0100

    install laravel

---
+++ database/migrations/2025_03_11_120458_create_courses_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->name();
+            $table->description();
+            $table->duration();
+            $table->level();
+            $table->category_id();
+            $table->sub_category();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('courses');
+    }
+};
---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+
+Route::middleware('auth:sanctum')
+    ->get('/api/user', function (Request $request) {
+        return $request->user();
+    });
