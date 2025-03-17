774e8958ada669867f40e8ec7a93d32cfdc84397
amissell <issellaya@gmail.com>
2025-03-17 12:48:17 +0100

    implements login and logout

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -26,12 +27,34 @@ class AuthController extends Controller
 
     public function login(Request $request)
     {
-      return 'login';
+      $request->validate([
+        'email'=> 'required|email|exists:users',
+        'password' => 'required'
+      ]);
+      
+      $user = User::where('email', $request->email)->first();
+
+      if (!$user || !Hash::check($request->password, $user->password))
+      {
+        return [
+          'message' => 'The provided credentials are incorrect!'
+        ];
+      }
+
+      $token = $user->createToken($user->name);
+      return [
+        'user' => $user,
+        'token' => $token->plainTextToken,
+      ];
     }
     
     public function logout(Request $request)
     {
-      return 'logout';
+      $request->user()->tokens()->delete();
+
+      return [
+        'message' => 'you are logged out !'
+      ];
     }
     
 }
--- routes/api.php
+++ routes/api.php
@@ -22,4 +22,4 @@ Route::apiResource('/tags',TagController::class);
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
